93691e51c22a9f90fcc291a4f2d39895138a4894
ThomasKoreman <thomaskoreman2001@gmail.com>
2019-06-12 13:35:23 +0200

    Update Buzz & Space
    Check ook even de TODO en XXX

--- src/main/java/nl/joekoei/tomorrowland/spaceCommand.java
+++ src/main/java/nl/joekoei/tomorrowland/spaceCommand.java
@@ -6,40 +6,43 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
 
 public class spaceCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("spacemountain")) {
-            if (sender.hasPermission("mw")) {
-                if (args.length == 0) {
-                    sender.sendMessage(ChatColor.RED + "You need to give arguments");
-                } else if (args.length == 1) {
-                    if (args[0].equalsIgnoreCase("power")) {
-                        Bukkit.broadcastMessage(args[0]);
-                        spaceBuilder m = new spaceBuilder();
-                        if (m.getMenu().getItem(1).equals(Material.RED_CONCRETE)) {
-                            ArrayList lore = new ArrayList();
-                            lore.add("ON");
-                            m.getMenu().getItem(1).setType(Material.GREEN_CONCRETE);
-                            m.getMenu().getItem(1).getItemMeta().setLore(lore);
-                        } else if (m.getMenu().getItem(1).equals(Material.GREEN_CONCRETE)) {
-                            ArrayList lore = new ArrayList();
-                            lore.add("OFF");
-                            m.getMenu().getItem(1).setType(Material.RED_CONCRETE);
-                            m.getMenu().getItem(1).getItemMeta().setLore(lore);
-                        }
-                    }
-                }
-            } else {
-                sender.sendMessage(ChatColor.RED + "You cant do this!");
-            }
-        }
-
-
-        return false;
-    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("spacemountain")) {
+			if (sender.hasPermission("operator.club33") || sender.hasPermission("operator.passholder")) {
+				if (args.length == 0) {
+					sender.sendMessage(ChatColor.RED + "You need to give arguments");
+				} else if (args.length == 1) {
+					if (args[0].equalsIgnoreCase("power")) {
+						Bukkit.broadcastMessage(args[0]);
+						spaceBuilder m = new spaceBuilder();
+						if (m.getMenu().getItem(1).getType().equals(Material.RED_CONCRETE)) {
+							m.getMenu().getItem(1).setType(Material.GREEN_CONCRETE);
+							// TODO: Setten van de title ("ON")
+							/**
+							 * ArrayList lore = new ArrayList(); lore.add("ON");
+							 * m.getMenu().getItem(1).setType(Material.GREEN_CONCRETE);
+							 * m.getMenu().getItem(1).getItemMeta().setLore(lore);
+							 */
+						} else if (m.getMenu().getItem(1).getType().equals(Material.GREEN_CONCRETE)) {
+							m.getMenu().getItem(1).setType(Material.RED_CONCRETE);
+							// TODO: Setten van de title ("OFF")
+							/**
+							 * ArrayList lore = new ArrayList(); lore.add("OFF");
+							 * m.getMenu().getItem(1).setType(Material.RED_CONCRETE);
+							 * m.getMenu().getItem(1).getItemMeta().setLore(lore);
+							 */
+						}
+					}
+				}
+			} else {
+				// XXX: Deze vorm van 'No permission' prima?
+				sender.sendMessage(
+						ChatColor.RED + "Hey!" + ChatColor.GRAY + "You have no permission to perform this action");
+			}
+		}
+		return false;
+	}
 }
