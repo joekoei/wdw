70ee0fbb8ad7b6b014e17924f8725515ed0418fd
Joekoei <info@joekoei.nl>
2019-06-12 10:54:50 +0200

    Update

--- src/main/java/nl/joekoei/control/controlBuilder.java
+++ src/main/java/nl/joekoei/control/controlBuilder.java
@@ -1,15 +1,22 @@
 package nl.joekoei.control;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
 
 public class controlBuilder {
 
     private Inventory menu;
-    private Inventory m;
 
     public controlBuilder() {
         menu = Bukkit.createInventory(null, 18, "Control Panels");
+        ItemStack i = new ItemStack(Material.ACACIA_BOAT);
+        ArrayList lore = new ArrayList();
+        lore.add("Splash Mountain");
+        i.getItemMeta().setLore(lore);
     }
 
     public Inventory getMenu(){
