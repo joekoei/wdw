a73fb44b31c72f40956391904aaa7767c75e4ab1
Joekoei <info@joekoei.nl>
2019-06-12 18:54:19 +0200

    Nu blijft iedereen met zn klauwe van de Names. Anders delete ik heel deze repo gewoon.

---
+++ src/main/java/eu/magicparksmc/waltdisneyworld/fastpass/fastpassInfo.java
@@ -0,0 +1,14 @@
+package eu.magicparksmc.waltdisneyworld.fastpass;
+
+public class fastpassInfo {
+
+    private String m;
+
+    public fastpassInfo(){
+        m = "Fastpass is not available Yet";
+    }
+
+    public String getM() {
+        return m;
+    }
+}
