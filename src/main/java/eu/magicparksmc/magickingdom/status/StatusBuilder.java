787e4dc8c9f60cdef19fd60df17858b70ecbce89
Joekoei <info@joekoei.nl>
2019-06-19 08:56:52 +0200

    Got some Status in it

--- src/main/java/eu/magicparksmc/magickingdom/status/StatusBuilder.java
+++ src/main/java/eu/magicparksmc/magickingdom/status/StatusBuilder.java
@@ -1,4 +1,28 @@
 package eu.magicparksmc.magickingdom.status;
 
+import org.bukkit.ChatColor;
+
 public class StatusBuilder {
+
+    private String opened;
+    private String closed;
+    private String maintenance;
+
+    public StatusBuilder() {
+        opened = ChatColor.GREEN + "Opened";
+        closed = ChatColor.RED + "Closed";
+        maintenance = ChatColor.GOLD + "Maintenance";
+    }
+
+    public String getOpened() {
+        return opened;
+    }
+
+    public String getClosed() {
+        return closed;
+    }
+
+    public String getMaintenance() {
+        return maintenance;
+    }
 }
