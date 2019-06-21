2726cc7da5bafd1908a29eb9359118bb28774c81
Joekoei <info@joekoei.nl>
2019-06-21 20:04:45 +0200

    Final Push Joey - 21-6-2019 20:05

--- src/main/java/nl/joekoei/ridecounter/RideCounterCommand.java
+++ src/main/java/nl/joekoei/ridecounter/RideCounterCommand.java
@@ -1,10 +1,10 @@
 package nl.joekoei.ridecounter;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import nl.joekoei.core.Core;
 
@@ -12,24 +12,18 @@ public class RideCounterCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (command.getName().equalsIgnoreCase("ridecounter")) {
-            if(sender instanceof Player){
-                Player player = (Player) sender;
                     if(args.length < 3){
                         RideCounterElement element = Core.getRidecounterConfig().getByPlayer(Bukkit.getPlayer(args[0]));
                         if(element != null){
                             element.count(args[1]);
                             Core.getRidecounterConfig().save();
-                            Bukkit.broadcastMessage(args[0] + " " + element.getData());
+                            Bukkit.broadcastMessage(args[0] + element.getData());
                         } else {
-                            // iets ging fout
                         }
                     } else {
-
+                        sender.sendMessage(ChatColor.RED + "Nope");
                     }
-                } else {
-                    //afblijven
                 }
-            }
         return false;
     }
 }
