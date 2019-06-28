cc6b22b4641ef16c17467157b84afe55b8efc84a
Joekoei <info@joekoei.nl>
2019-06-28 20:18:44 +0200

    Alle Lists aangepast met Attractie's.

--- src/main/java/eu/magicparksmc/enums/magickingdom/FantasylandList.java
+++ src/main/java/eu/magicparksmc/enums/magickingdom/FantasylandList.java
@@ -2,50 +2,69 @@ package eu.magicparksmc.enums.magickingdom;
 
 public class FantasylandList {
 	enum fantasyland {
-		BIBBIDI_BOBBIDI_BOUTIQUE("Bibbidi Bobbidi Boutique"), BIG_TOP_SOUVENIRS("Big Top Souvenirs"),
-		FANTASY_FAIRE("Fantasy Faire");
+		BIBBIDI_BOBBIDI_BOUTIQUE("Bibbidi Bobbidi Boutique", ""),
+		BIG_TOP_SOUVENIRS("Big Top Souvenirs", ""),
+		FANTASY_FAIRE("Fantasy Faire", "");
 		String fantasylandName;
+		String fantasylandDescription;
 
-		fantasyland(String name) {
+		fantasyland(String name, String description) {
 			this.fantasylandName = name;
+			this.fantasylandDescription = description;
 		}
 	}
 
 	enum fantasylandRides {
-		ITS_A_SMALL_WORLD("'it's a small world'"), PETER_PANS_FLIGHT("Peter Pan's Flight"),
-		PRINCE_CHARMING_CARROUSEL("Prince Charming Regal Carrousel"),
-		WINNIE_THE_POOH("The Many Adventures of Winnie the Pooh"), MAD_TEA_PARTY("Mad Tea Party"),
-		SEVEN_DWARFS_MINE_TRAIN("Seven Dwarfs Mine Train"),
-		JOURNEY_OF_THE_LITTLE_MERMAID("Under the Sea - Journey of the Little Mermaid"),
-		DUMBO_THE_FLYING_ELEPHANT("Dumbo the Flying Elephant"), BARNSTORMER("The Barnstormer");
+		ITS_A_SMALL_WORLD("'it's a small world'", ""),
+		PETER_PANS_FLIGHT("Peter Pan's Flight", ""),
+		PRINCE_CHARMING_CARROUSEL("Prince Charming Regal Carrousel", ""),
+		WINNIE_THE_POOH("The Many Adventures of Winnie the Pooh", ""),
+		MAD_TEA_PARTY("Mad Tea Party", ""),
+		SEVEN_DWARFS_MINE_TRAIN("Seven Dwarfs Mine Train", ""),
+		JOURNEY_OF_THE_LITTLE_MERMAID("Under the Sea - Journey of the Little Mermaid", ""),
+		DUMBO_THE_FLYING_ELEPHANT("Dumbo the Flying Elephant", ""),
+		BARNSTORMER("The Barnstormer", "");
 		String fantasylandName;
+		String fantasylandDescription;
 
-		fantasylandRides(String name) {
+		fantasylandRides(String name, String description) {
 			this.fantasylandName = name;
+			this.fantasylandDescription = description;
 		}
 	}
 
 	enum fantasylandAttractions {
-		MICKEYS_PHILHARMAGIC("Mickey's PhilharMagic"), ENCHANTED_TALES_WITH_BELLE("Enchanted Tales with Belle"),
-		PRINCESS_FAIRYTALE_HALL("Princess Fairytale Hall"), FAIRYTALE_GARDEN("Fairytale Garden"),
-		ARIELS_GROTTO("Ariels Grotto"), SPLASH_N_SOAK_STATION("Casey Jr. Splash 'N' Soak Station"),
-		PETES_SILLY_SIDESHOW("Pete's Silly Sideshow");
+		MICKEYS_PHILHARMAGIC("Mickey's PhilharMagic", ""),
+		ENCHANTED_TALES_WITH_BELLE("Enchanted Tales with Belle", ""),
+		PRINCESS_FAIRYTALE_HALL("Princess Fairytale Hall", ""),
+		FAIRYTALE_GARDEN("Fairytale Garden", ""),
+		ARIELS_GROTTO("Ariels Grotto", ""),
+		SPLASH_N_SOAK_STATION("Casey Jr. Splash 'N' Soak Station", ""),
+		PETES_SILLY_SIDESHOW("Pete's Silly Sideshow", "");
 		String fantasylandName;
+		String fantasylandDescription;
 
-		fantasylandAttractions(String name) {
+		fantasylandAttractions(String name, String description) {
 			this.fantasylandName = name;
+			this.fantasylandDescription = description;
 		}
 	}
 
 	enum fantasylandRestaurants {
-		PINOCCHIO_VILLAGE_HAUS("Pinocchio Village Haus"), CINDERELLAS_ROYAL_TABLE("Cinderella's Royal Table"),
-		THE_FRIARS_NOOK("The Friars Nook"), STORYBOOK_TREATS("Storybook Treats"),
-		BE_OUR_GUEST("Be Our Guest Restaurant"), GASTONS_TAVERN("Gaston's Tavern"), CHESHIRE_CAFE("Cheshire Café"),
-		PRINCE_ERICS_VILLAGE_MARKET("Prince Eric's Village Market");
+		PINOCCHIO_VILLAGE_HAUS("Pinocchio Village Haus", ""),
+		CINDERELLAS_ROYAL_TABLE("Cinderella's Royal Table", ""),
+		THE_FRIARS_NOOK("The Friars Nook", ""),
+		STORYBOOK_TREATS("Storybook Treats", ""),
+		BE_OUR_GUEST("Be Our Guest Restaurant", ""),
+		GASTONS_TAVERN("Gaston's Tavern", ""),
+		CHESHIRE_CAFE("Cheshire Café", ""),
+		PRINCE_ERICS_VILLAGE_MARKET("Prince Eric's Village Market", "");
 		String fantasylandName;
+		String fantasylandDescription;
 
-		fantasylandRestaurants(String name) {
+		fantasylandRestaurants(String name, String description) {
 			this.fantasylandName = name;
+			this.fantasylandDescription = description;
 		}
 	}
 }
--- src/main/java/eu/magicparksmc/enums/magickingdom/LibertySquareList.java
+++ src/main/java/eu/magicparksmc/enums/magickingdom/LibertySquareList.java
@@ -11,31 +11,40 @@ public class LibertySquareList {
 	}
 
 	enum libertysquareRides {
-		LIBERTY_SQUARE_RIVERBOAT("Liberty Square Riverboat"), HAUNTED_MANSION("Haunted Mansion");
+		LIBERTY_SQUARE_RIVERBOAT("Liberty Square Riverboat", ""),
+		HAUNTED_MANSION("Haunted Mansion", "");
 		String libertysquareName;
+		String libertysquareDescription;
 
-		libertysquareRides(String name) {
+		libertysquareRides(String name, String description) {
 			this.libertysquareName = name;
+			this.libertysquareDescription = description;
 		}
 	}
 
 	enum libertysquareAttractions {
-		HALL_OF_PRESIDENTS("The Hall of Presidents");
+		HALL_OF_PRESIDENTS("The Hall of Presidents", "");
 		String libertysquareName;
+		String libertysquareDescription;
 
-		libertysquareAttractions(String name) {
+		libertysquareAttractions(String name, String description) {
 			this.libertysquareName = name;
+			this.libertysquareDescription = description;
 		}
 	}
 
 	enum libertysquareRestaurants {
-		DIAMOND_HORSESHOE("The Diamond Horseshoe"), LIBERTY_TREE_TAVERN("Liberty Tree Tavern"),
-		SLEEPY_HOLLOW("Sleepy Hollow"), LIBERTY_SQUARE_MARKET("Liberty Square Market"),
-		COLUMBIA_HARBOUR_HOUSE("Columbia Harbour House");
+		DIAMOND_HORSESHOE("The Diamond Horseshoe", ""),
+		LIBERTY_TREE_TAVERN("Liberty Tree Tavern", ""),
+		SLEEPY_HOLLOW("Sleepy Hollow", ""),
+		LIBERTY_SQUARE_MARKET("Liberty Square Market", ""),
+		COLUMBIA_HARBOUR_HOUSE("Columbia Harbour House", "");
 		String libertysquareName;
+		String libertysquareDescription;
 
-		libertysquareRestaurants(String name) {
+		libertysquareRestaurants(String name, String description) {
 			this.libertysquareName = name;
+			this.libertysquareDescription = description;
 		}
 	}
 }
